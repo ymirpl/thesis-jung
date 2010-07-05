d2aee2528923849c40c725d1c0e6d9708477ba8e
Marcin Mincer <marcin.mincer@gmail.com>
2010-07-06 00:58:27 +0200

    first measure

--- src/org/thesis/cdr/BigFile.java
+++ src/org/thesis/cdr/BigFile.java
@@ -3,61 +3,51 @@ package org.thesis.cdr;
 import java.util.*;
 import java.io.*;
 
-/** 
- * According to http://code.hammerpig.com/how-to-read-really-large-files-in-java.html
+/**
+ * According to
+ * http://code.hammerpig.com/how-to-read-really-large-files-in-java.html
+ * 
  * @author hammerpig.com
- *
+ * 
  */
 
-public class BigFile implements Iterable<String>
-{
-    private BufferedReader _reader;
- 
-    public BigFile(String filePath) throws Exception
-    {
-	_reader = new BufferedReader(new FileReader(filePath));
-    }
- 
-    public void Close()
-    {
-	try
-	{
-	    _reader.close();
+public class BigFile implements Iterable<String> {
+	private BufferedReader _reader;
+
+	public BigFile(String filePath) throws Exception {
+		_reader = new BufferedReader(new FileReader(filePath));
 	}
-	catch (Exception ex) {}
-    }
- 
-    public Iterator<String> iterator()
-    {
-	return new FileIterator();
-    }
- 
-    private class FileIterator implements Iterator<String>
-    {
-	private String _currentLine;
- 
-	public boolean hasNext()
-	{
-	    try
-	    {
-		_currentLine = _reader.readLine();
-	    }
-	    catch (Exception ex)
-	    {
-		_currentLine = null;
-		ex.printStackTrace();
-	    }
- 
-	    return _currentLine != null;
+
+	public void Close() {
+		try {
+			_reader.close();
+		} catch (Exception ex) {
+		}
 	}
- 
-	public String next()
-	{
-	    return _currentLine;
+
+	public Iterator<String> iterator() {
+		return new FileIterator();
 	}
- 
-	public void remove()
-	{
+
+	private class FileIterator implements Iterator<String> {
+		private String _currentLine;
+
+		public boolean hasNext() {
+			try {
+				_currentLine = _reader.readLine();
+			} catch (Exception ex) {
+				_currentLine = null;
+				ex.printStackTrace();
+			}
+
+			return _currentLine != null;
+		}
+
+		public String next() {
+			return _currentLine;
+		}
+
+		public void remove() {
+		}
 	}
-    }
 }
--- src/org/thesis/cdr/CDR.java
+++ src/org/thesis/cdr/CDR.java
@@ -1,11 +1,12 @@
 package org.thesis.cdr;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 import org.apache.commons.collections15.Factory;
 
 import edu.uci.ics.jung.algorithms.importance.AbstractRanker;
-import edu.uci.ics.jung.algorithms.importance.BetweennessCentrality;
+import edu.uci.ics.jung.algorithms.scoring.BetweennessCentrality;
 import edu.uci.ics.jung.algorithms.scoring.DegreeScorer;
 import edu.uci.ics.jung.algorithms.scoring.VertexScorer;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
@@ -17,16 +18,18 @@ import edu.uci.ics.jung.io.PajekNetReader;
 public class CDR {
 
 
-	public static DirectedSparseGraph<String, Number> Graph;
-	public static BigFile bFile;
+	public DirectedSparseGraph<String, Number> Graph;
+	private BigFile bFile;
 	public static String FILE = "/media/Right/cdr-slice.txt";
-	
+	private FileWriter saveFile;
+	private BufferedWriter output;
+	List<String> vertices;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		CDR myCDR = new CDR();
 		myCDR.readData();
 		myCDR.insertNodes();
+		myCDR.computeBetweennessCentrality("bet.txt");
 
 
 	}
@@ -46,17 +49,51 @@ public class CDR {
 			
 			System.out.println(line);
 		}
+		vertices = new ArrayList<String>(Graph.getVertices());
 	}
 	
+	public void computeBetweennessCentrality(String outFilename) {
+		BetweennessCentrality<String, Number> bc = new BetweennessCentrality<String, Number>(
+				Graph);
+
+		openSaveFile(outFilename);
+
+		for (String v : vertices) {
+			try {
+				output.write(v + " " + bc.getVertexScore(v).toString() + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}	
 	
 	public void readData() {
 		// a lot lots to read...
 		try {
 			bFile = new BigFile(FILE);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	public void openSaveFile(String filename) {
+		try {
+			saveFile = new FileWriter(filename);
+			output = new BufferedWriter(saveFile);
+			
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+	
+	public void closeSaveFile() {
+		try {
+			output.close();
+		} catch (IOException e) {
+			System.err.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}	
 
 }
